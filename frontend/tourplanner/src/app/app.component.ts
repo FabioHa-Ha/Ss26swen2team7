0bfd631e6a492097c74ad7a464cdc29180f7e4a6
Aaron Domingo <daaronjhay@gmx.at>
2026-03-09 13:04:17 +0100

    successfully integrated spartan.ng to Angular project

--- frontend/tourplanner/src/app/app.component.ts
+++ frontend/tourplanner/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmButtonImports],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'tourplanner';
