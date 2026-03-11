197de6c961c4ddf746d3593030709cb519241e31
Aaron Domingo <daaronjhay@gmx.at>
2026-03-11 10:45:29 +0100

    defined routes for the components

--- frontend/tourplanner/src/app/app.routes.ts
+++ frontend/tourplanner/src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./components/auth/register/register.component').then(m => m.RegisterComponent)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./components/auth/login/login.component').then(m => m.LoginComponent)
+    },
+    {
+        path: 'tours',
+        loadComponent: () => import('./components/layout/main-layout/main-layout.component').then(m => m.MainLayoutComponent),
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./components/tours/tour-list/tour-list.component').then(m => m.TourListComponent)
+            },
+            {
+                path: 'dashboard',
+                loadComponent: () => import('./components/dashboard/dashboard.component').then(m => m.DashboardComponent)
+            },
+            {
+                path: 'search',
+                loadComponent: () => import('./components/search/search-panel/search-panel.component').then(m => m.SearchPanelComponent)
+            },
+            {
+                path: ':id',
+                loadComponent: () => import('./components/tours/tour-detail/tour-detail.component').then(m => m.TourDetailComponent)
+            }
+        ]
+    }
+];
