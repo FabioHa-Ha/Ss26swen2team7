e4dacd81405c6eb325c8e4c76c166290a4d3075b
Aaron Domingo <daaronjhay@gmx.at>
2026-04-02 14:16:45 +0200

    added onSubmit()

--- frontend/tourplanner/src/app/components/auth/login/login.component.ts
+++ frontend/tourplanner/src/app/components/auth/login/login.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  email = '';
+  password = '';
 
+  onSubmit(): void {
+    console.log(this.email, this.password);
+  }
 }
