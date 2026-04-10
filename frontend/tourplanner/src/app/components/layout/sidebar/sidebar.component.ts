be49306f57e1656c32ee1d8583b0dbd13f87880b
Aaron Domingo <daaronjhay@gmx.at>
2026-04-10 16:14:07 +0200

    added reusable UI components

--- frontend/tourplanner/src/app/components/layout/sidebar/sidebar.component.ts
+++ frontend/tourplanner/src/app/components/layout/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
@@ -11,11 +12,55 @@ export class SidebarComponent {
   // readonly collapsed = input(false);
   // readonly toggleCollapse = output<void>();
   
+  someTours = [
+    {
+      id: '1',
+      name: 'Vienna City Ride',
+      transportType: 'bike',
+      distance: 18,
+      duration: 95,
+    },
+    {
+      id: '2',
+      name: 'Danube Hiking Trail',
+      transportType: 'hike',
+      distance: 12,
+      duration: 180,
+    },
+    {
+      id: '3',
+      name: 'Prater Park Run',
+      transportType: 'running',
+      distance: 5,
+      duration: 28,
+    },
+    {
+      id: '4',
+      name: 'Alpine Weekend Trip',
+      transportType: 'vacation',
+      distance: 240,
+      duration: 600,
+    }
+  ];
+
   collapsed = false;
+  isMobileOpen = false;
+
+  openMobile() {
+    this.isMobileOpen = true;
+  }
+
+  closeMobile() {
+    this.isMobileOpen = false;
+  }
 
   toggleSidebar() {
-    this.collapsed = !this.collapsed;
+    this.collapsed = !this.collapsed; // desktop only
   }
+
+  // toggleSidebar() {
+  //   this.collapsed = !this.collapsed;
+  // }
 
   // readonly tourService = inject(TourService);
   // private readonly authService = inject(AuthService);
@@ -47,6 +92,24 @@ export class SidebarComponent {
 
   setFilter(type: string | 'all'): void {
     this.selectedType = type;
+  }
+
+  formatTime(minutes: number): string {
+    if (minutes == null || isNaN(Number(minutes))) {
+      return '-';
+    }
+
+    const hours = Math.floor(minutes / 60);
+    const mins = minutes % 60;
+
+    if (hours === 0) {
+      return `${mins}m`;
+    }
+
+    if (mins === 0) {
+      return `${hours}h`;
+    }
+    return `${hours}h ${mins}m`;
   }
 
   exportData(): void {
