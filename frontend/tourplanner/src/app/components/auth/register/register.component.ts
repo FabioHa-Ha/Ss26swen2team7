0d7abc4a9567c506acad61dd155c147772847776
Aaron Domingo <daaronjhay@gmx.at>
2026-04-12 11:53:55 +0200

    added onSubmit()

--- frontend/tourplanner/src/app/components/auth/register/register.component.ts
+++ frontend/tourplanner/src/app/components/auth/register/register.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  username = '';
+  password = '';
 
+  constructor (private authService: AuthService, private router: Router) {}
+
+
+  onSubmit(): void {
+    this.authService.register({ username: this.username, password: this.password }).subscribe({
+      next: () => this.router.navigate(['/login']),
+      error: (err) => console.error('Registration failed', err)
+    });
+  }
 }
