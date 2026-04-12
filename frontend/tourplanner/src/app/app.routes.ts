1176a80faa05d15892d5ac629fba804d84d8f35f
Aaron Domingo <daaronjhay@gmx.at>
2026-04-12 15:00:03 +0200

    added authGuard

--- frontend/tourplanner/src/app/app.routes.ts
+++ frontend/tourplanner/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './services/guards/auth.guard';
 
 export const routes: Routes = [
     {
@@ -16,6 +17,7 @@ export const routes: Routes = [
     },
     {
         path: 'tours',
+        canActivate: [authGuard],
         loadComponent: () => import('./components/layout/main-layout/main-layout.component').then(m => m.MainLayoutComponent),
         children: [
             {
