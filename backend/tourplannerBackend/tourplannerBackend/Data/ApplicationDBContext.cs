10cb87f35ee921d65abe92a4ab46964e16d8ba11
Fabio-Ha-Ha <if24b506@technikum-wien.at>
2026-04-14 23:22:40 +0200

    Seeds Database with Transport Types

--- backend/tourplannerBackend/tourplannerBackend/Data/ApplicationDBContext.cs
+++ backend/tourplannerBackend/tourplannerBackend/Data/ApplicationDBContext.cs
@@ -13,6 +13,18 @@ namespace tourPlannerBackend.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TransportType>().HasData(
+                new TransportType { Id = 1, Name = "Hike" },
+                new TransportType { Id = 2, Name = "Bike" },
+                new TransportType { Id = 3, Name = "Running" },
+                new TransportType { Id = 4, Name = "Vacation" }
+            );
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<TransportType> TransportTypes { get; set; }
         public DbSet<Tour> Tours { get; set; }
---
+++ backend/tourplannerBackend/tourplannerBackend/Migrations/20260414212032_TransportTypeSeeding.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace tourplannerBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class TransportTypeSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TransportTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Hike" },
+                    { 2, "Bike" },
+                    { 3, "Running" },
+                    { 4, "Vacation" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TransportTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TransportTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "TransportTypes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "TransportTypes",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
