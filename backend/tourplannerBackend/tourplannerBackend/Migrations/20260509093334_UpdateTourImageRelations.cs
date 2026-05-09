bb95250d31c04d68bc031acaed1c9be402a75f03
Aaron Domingo <daaronjhay@gmx.at>
2026-05-09 12:18:17 +0200

    created

---
+++ backend/tourplannerBackend/tourplannerBackend/Migrations/20260509093334_UpdateTourImageRelations.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tourplannerBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTourImageRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                table: "Tours");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TourId",
+                table: "TourImages",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TourImages_TourId",
+                table: "TourImages",
+                column: "TourId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TourImages_Tours_TourId",
+                table: "TourImages",
+                column: "TourId",
+                principalTable: "Tours",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TourImages_Tours_TourId",
+                table: "TourImages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TourImages_TourId",
+                table: "TourImages");
+
+            migrationBuilder.DropColumn(
+                name: "TourId",
+                table: "TourImages");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ImageId",
+                table: "Tours",
+                type: "integer",
+                nullable: true);
+        }
+    }
+}
---
+++ backend/tourplannerBackend/tourplannerBackend/Migrations/20260509094217_FixTourImageModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tourplannerBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTourImageModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TourLog",
+                table: "TourImages");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TourLog",
+                table: "TourImages",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
