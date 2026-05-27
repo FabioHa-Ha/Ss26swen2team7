dd241c68c34b30707ff1e73a63c6766dd1342616
Aaron Domingo <daaronjhay@gmx.at>
2026-05-27 16:51:33 +0200

    added orsApiKey

--- frontend/tourplanner/src/environments/environment.ts
+++ frontend/tourplanner/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:4200/api'
+    apiUrl: 'http://localhost:4200/api',
+    orsApiKey: 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6Ijk0N2ZmZmIzN2Q0NzQwNWZiMDdhODU4ZmQ5MmY2ZTZhIiwiaCI6Im11cm11cjY0In0='
 }
